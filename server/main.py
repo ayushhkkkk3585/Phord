43061de6a1d3cef2c714b53a686cf00373aad7c2
ayushhkkkk3585 <ayushkhalate04@gmail.com>
2024-10-26 12:55:01 +0530

    ayush commit

--- server/main.py
+++ server/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://phord-nine.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
